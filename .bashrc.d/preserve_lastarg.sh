b3b6d21169ba5051bd4164083f0d68d0e53f731a
Kevin Pulo <kev@pulo.com.au>
2023-03-14 10:16:26 +1100

    `$'...'` doesn't expand inside `""`
    https://github.com/devkev/bin/commit/93c656afeecb13296c694d722b824ca7d4492af9#r104220645

--- .bashrc.d/preserve_lastarg.sh
+++ .bashrc.d/preserve_lastarg.sh
@@ -1,6 +1,6 @@
 # bashrc
 
-PROMPT_COMMAND='_preserve_lastarg="$_"'"$'\n'$PROMPT_COMMAND"
+PROMPT_COMMAND='_preserve_lastarg="$_"'$'\n'"$PROMPT_COMMAND"
 bind -x '"\xC0\1":: "$_preserve_lastarg"'
 bind '"\xC0\2": accept-line'
 bind '"\r": "\xC0\1\xC0\2"'
