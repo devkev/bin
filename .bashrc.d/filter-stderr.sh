67c10d57325186b35bf6bd76a9dc55047c6c62a8
Kevin Pulo <kev@pulo.com.au>
2023-04-11 17:56:42 +1000

    add filter-stderr function

---
+++ .bashrc.d/filter-stderr.sh
@@ -0,0 +1,37 @@
+
+# Usage:
+#   filter-stderr cmd_to_run arg1 arg2 ... --- stderr_filter_cmd arg1 arg2 ...
+# If '---' somehow conflicts with an actual arg you need to pass to cmd_to_run or stderr_filter,
+# then set $FILTER_STDERR_DELIM to the delimiter that you'd like to use.
+
+function filter-stderr {
+	local delim="${FILTER_STDERR_DELIM:----}"
+	local -a cmd
+	local -a filter
+	local i
+	local target=cmd
+	for i; do
+		case "$i" in
+			"$delim")
+				target="filter"
+				;;
+			*)
+				# FIXME: there's a better way to do this. oh well.
+				eval local -a "$target"'+=("$i")'
+				;;
+		esac
+	done
+	#declare -p cmd filter
+	# it might be possible to use just tmp, ie. not need both tmp and tmp2.
+	# but having both doesn't hurt, and means i can avoid having to think about it.
+	local tmp tmp2
+	( "${cmd[@]}"  {tmp}>&1 1>&2 2>&$tmp {tmp}>&- | "${filter[@]}" ) {tmp2}>&1 >&2 2>&$tmp2 {tmp2}>&-
+}
+export -f filter-stderr
+
+function stdouterr-test {
+	echo this is stdout
+	echo this is stderr 1>&2
+}
+export -f stdouterr-test
+
